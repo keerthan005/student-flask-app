2e575acf87ae22bb43e0b6eabe33d937abcb865e
keerthan <keerthankumar428.com>
2025-06-30 15:54:11 +0530

    Fix: moved model loading into route

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Load the trained model
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return "Student Performance Prediction Flask API is running!"
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
 
-    # Load model here instead of globally
-    with open('model.pkl', 'rb') as f:
-        model = pickle.load(f)
-
-    input_features = [data['gender'], data['race_ethnicity'], data['parental_level_of_education'],
-                      data['lunch'], data['test_preparation_course'],
-                      data['reading_score'], data['writing_score']]
+    input_features = [data['gender'],
+                      data['race_ethnicity'],
+                      data['parental_level_of_education'],
+                      data['lunch'],
+                      data['test_preparation_course'],
+                      data['reading_score'],
+                      data['writing_score']]
 
     prediction = model.predict([input_features])
     return jsonify({'Predicted Class': int(prediction[0])})
